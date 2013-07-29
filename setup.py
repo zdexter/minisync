ec36245a7c1fbc25bfca199ea020cf851344d82c
Zach Dexter <zach@tutorspree.com>
2013-07-29 10:40:29 -0400

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Crudad',
+      version='0.1',
+      description='Object synchronization library for SQLAlchemy',
+      author='Tutorspree',
+      author_email='info@tutorspree.com',
+      url='https://github.com/Tutorspree/crudad/',
+      packages=['distutils', 'distutils.command'],
+     )
